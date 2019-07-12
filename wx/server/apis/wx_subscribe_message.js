a228cb3e4769115e15e4551d190509b97aef9222
mimiddi <1490533119@qq.com>
2019-07-12 14:34:32 +0800

    1

--- wx/server/apis/wx_subscribe_message.js
+++ wx/server/apis/wx_subscribe_message.js
@@ -7,8 +7,8 @@ function WXSubscribeMessage() {
 	var tool = new tools;
 	var query = tool.query;
     this.Run = async function (type, param){
-		// console.info(type)
-		// console.info(param)
+		console.info(type)
+		console.info(param)
 		var row = ''
 		var sql = ''
 		sql = "select message from subscribe_message order by sort"
@@ -19,15 +19,24 @@ function WXSubscribeMessage() {
 	}
 }
 	
-function sendText(openid,messages,index){
+function sendText(param,messages,index){
 	if(index < messages.length){
 		let message = messages[index].message
-		api.sendText(openid, message, function(err,result){
+		api.sendText(JSON.parse(param).openid, message, function(err,result){
 			console.info(result)
 			if(result.errcode == 0){
-				sendText(openid,messages,index+1)
+				sendText(param,messages,index+1)
 			}
 		});
+	}else{
+		if(JSON.parse(param).eventkey.length > 0){
+			let message = '桌号:' + JSON.parse(param).eventkey.split('_')[1]
+			if(message.length > 0){
+				api.sendText(JSON.parse(param).openid, message, function(err,result){
+					console.info(result)
+				});
+			}
+		}
 	}
 }
 
