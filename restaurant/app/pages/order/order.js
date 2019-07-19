d5c1da7809228f5c830f932e16f356671d238bfc
mimiddi <1490533119@qq.com>
2019-07-19 16:59:03 +0800

    1

--- restaurant/app/pages/order/order.js
+++ restaurant/app/pages/order/order.js
@@ -7,62 +7,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cart: [],
+    totalPrice: 0,
+    styleList: ['堂食', '外带'],
+    style: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    console.info(app.globalData.cart)
+  onLoad: function(options) {
+    this.getCart()
+  },
+
+  getCart: function() {
+    let cart = app.globalData.cart
+    let totalPrice = 0
+    cart = cart.map(function(eData) {
+      eData.subTotalPrice = eData.price * eData.number
+      totalPrice = totalPrice + eData.subTotalPrice
+      return eData
+    })
+    this.setData({
+      cart: cart,
+      totalPrice: totalPrice
+    })
+    console.info(this.data.cart)
+  },
+
+  selectStyle: function(e) {
+    this.setData({
+      style: e.currentTarget.dataset.id
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
