0615d505631c9b75e687a580e69d6ad5b2b06ff6
mimiddi <1490533119@qq.com>
2019-07-20 15:19:50 +0800

    1

---
+++ restaurant/admin/api/shop_getUploadToken.js
@@ -0,0 +1,36 @@
+// var qiniu = require("qiniu");
+// const qiniuConfig = require("./../config/qiniuConfig")
+var getUploadToken = require("./../utils/qiniuUploadToken")
+
+function shopGetUploadToken() {
+    this.Service = async function (version, param, callback) {
+        var data = {}
+        try {
+            let token = getUploadToken(param['key'])
+            data.uploadToken = token
+            data.key = param['key']
+            data.tempFilePath = param['tempFilePath']
+
+            // var accessKey = qiniuConfig.accessKey;
+            // var secretKey = qiniuConfig.secretKey;
+            // var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+            // var bucket = qiniuConfig.bucket;
+            //
+            // var keyToOverwrite = param['key'];
+            // var options = {
+            //     scope: bucket + ":" + keyToOverwrite,
+            // };
+            // var putPolicy = new qiniu.rs.PutPolicy(options);
+            // var uploadToken = putPolicy.uploadToken(mac);
+            // data.uploadToken = uploadToken
+            // data.key = param['key']
+            // data.tempFilePath = param['tempFilePath']
+
+            return callback(data);
+        } catch (e) {
+            console.info('boom!!!!!!!!!!!!!')
+        }
+    }
+}
+
+module.exports = shopGetUploadToken;
