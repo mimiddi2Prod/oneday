83ecd5304ebfb353007019aa42c53617dee12453
mimiddi <1490533119@qq.com>
2019-07-26 15:52:13 +0800

    1

---
+++ restaurant/admin/router.js
@@ -0,0 +1,34 @@
+function Router() {
+    this.Service = function (data, url, callback) {
+        var baseApi = {};
+        var arr = url.split("/");
+        var version = arr[1];
+        var apiFunction = arr[2];
+
+        switch (apiFunction) {
+            case "login":
+                var shopLogin = require("./api/login.js");
+                baseApi = new shopLogin;
+                break;
+            case "get_menu":
+                var getMenu = require("./api/get_menu.js");
+                baseApi = new getMenu;
+                break;
+            case "get_goods":
+                var getGoods = require("./api/get_goods.js");
+                baseApi = new getGoods;
+                break;
+            case "get_category":
+                var getCategory = require("./api/get_category.js");
+                baseApi = new getCategory;
+                break;
+            default:
+                callback({code: 4, data: {}, error: "api错误"});
+                return;
+        }
+
+        return baseApi.Service(version, data, callback);
+    }
+}
+
+module.exports = Router;
