5bebf38c4d3d3f7d5006ea34d98089fd56f23eb0
mimiddi <1490533119@qq.com>
2019-08-07 11:50:15 +0800

    1

--- restaurant/admin/api/yinbao.js
+++ restaurant/admin/api/yinbao.js
@@ -14,15 +14,17 @@ function yinbao() {
             let current_time = fm(new Date())
             let postData = {
                 "appId": appId,
-                "payMethod": "Cash",
+                "payMethod": "Wxpay",
                 // "customerNumber": "001",
-                "customerNum":"001",
+                // "customerNum":"155893853835",
                 "shippingFee":15.00,
                 "orderRemark": "addOnLineOrder",
                 "orderDateTime": current_time,
                 "contactAddress": "测试测试。。。。",
                 "contactName": "张三",
                 "contactTel": "1360097865",
+                "deliveryType":0,
+                "payOnLine":1,
                 // "deliveryType": 1,
                 // "dinnersNumber": 5,
                 // "restaurantAreaName": "一楼",
@@ -30,13 +32,19 @@ function yinbao() {
                 // "reservationTime": current_time,
                 "items": [
                     {
-                        "productUid": 1908061724233,
+                        "productUid": 491535479940357172,
                         "comment": "测试添加",
                         "quantity": 1,
-                        // "manualSellPrice":30.2
+                        "manualSellPrice":30.2
                     }
                 ]
             }
+            // let postData = {
+            //     "appId": appId,
+            // }
+            // let postData = {
+            //     "appId": appId,
+            // }
             let postDataJson = JSON.stringify(postData)
             let appKey = '718070985931782035'
             let sign = md5(appKey + postDataJson).toUpperCase()
@@ -45,7 +53,9 @@ function yinbao() {
             var options = {
                 host: 'area8-win.pospal.cn',
                 port: 443,
-                path: '/pospal-api2/openapi/v1/customerOpenApi/queryByNumber',
+                path: '/pospal-api2/openapi/v1/orderOpenApi/addOnLineOrder',
+                // path: '/pospal-api2/openapi/v1/cashierOpenApi/queryAllCashier',
+                // path: '/pospal-api2/openapi/v1/productOpenApi/queryProductImagePages',
                 method: 'POST',
                 form: postDataJson,
                 headers: {
@@ -104,15 +114,7 @@ async function HttpsPost(option, postData, sign) {
                 // console.info(data)
             })
             res.on('end', function (e) {
-                const contentType = res.headers['content-type'];
-                // if (!contentType.includes('image')) {
-                //     // console.log('获取小程序码图片失败，微信api返回的json为：')
-                //     // console.log(JSON.parse(data))
-                //     return resolve(null);
-                // }
-                // const imgBuffer = Buffer.from(data, 'binary');
-                // resolve({imgBuffer, contentType});
-                // resolve(imgBuffer)
+                // const contentType = res.headers['content-type'];
                 resolve(data)
             })
         })
