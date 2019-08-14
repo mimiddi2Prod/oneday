dd27c6d88775afbeff652ca8789b554e14f48b09
mimiddi <1490533119@qq.com>
2019-08-14 15:49:06 +0800

    1

--- shop/admin/js/utils/apis.js
+++ shop/admin/js/utils/apis.js
@@ -1,13 +1,8 @@
 var api = {
     getMenu: '../api/get_menu',
+    updateGoodsState: '../api/update_goodsState',
     getGoods: '../api/get_goods',
     getCategory: '../api/get_category',
-    updateGoodsStatus: '../api/update_goodsStatus',
-
-    addCategory: '../api/add_category',
-    updateCategory: '../api/update_category',
-    delCategory: '../api/del_category',
-    getOrder: '../api/get_order',
-    getOrderBySearch: '../api/get_order_by_search',
-    updateGoods:'../api/update_goods',
+    updateGoodsCategory: '../api/update_goods_category',
+    updateGoodsPrice: '../api/update_goods_price',
 }
