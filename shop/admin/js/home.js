f7a87d9f5a0d9b42b6a7ae39083c5652de20e50d
mimiddi <1490533119@qq.com>
2019-08-30 11:05:16 +0800

    1

--- shop/admin/js/home.js
+++ shop/admin/js/home.js
@@ -27,6 +27,7 @@ $(document).ready(function () {
     getWaitShip()
     getAfterSaleNumber()
     getSales()
+    writeChart()
 })
 
 function getWaitShip() {
@@ -54,4 +55,18 @@ function getSales() {
         console.info(res)
         homeVM.sales = res.number
     })
+}
+
+function writeChart() {
+    // var data = [[1,2,3,4,5,6,7,8,9],[3,6,8,1,11,22,4,21,6]];
+    var data = [[3,6,8,1,11,22,4,21,6,1,2,3,4,5,6,7,8,9]];
+    var data_max = 30; //Y轴最大刻度
+    var line_title = ["A","B"]; //曲线名称
+    var y_label = "数量"; //Y轴标题
+    var x_label = "天数"; //X轴标题
+    var x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]; //定义X轴刻度值
+    var title = "这是标题"; //统计图标标题
+    j.jqplot.diagram.base("chart1", data, line_title, title, x, x_label, y_label, data_max, 1);
+    // j.jqplot.diagram.base("chart2", data, line_title, "这是统计标题", x, x_label, y_label, data_max, 2);
+
 }
