3c09b2f31182d564d6cb9046e4c6f0f38e20e705
mimiddi <1490533119@qq.com>
2019-09-10 11:45:17 +0800

    1

---
+++ restaurant/server/apis/restaurant_payfee.js
@@ -0,0 +1,56 @@
+var tools = require("./../tool");
+
+function RestaurantPayfee() {
+    var tool = new tools;
+    var log = tool.log;
+
+    this.Run = async function (ver, param, res) {
+        var name = "RestaurantPayfee::Run";
+        log.debug("RestaurantPayfee::Run.in");
+        var data = {};
+        var response = tool.error.OK;
+
+        if (!param["openid"]) {
+            log.warn(name, 'openid is not defined')
+        } else if (!param["money"]) {
+            log.warn(name, 'money! money! money! is not defined')
+        } else {
+            try {
+                var payData = {}
+                payData.openid = param['openid']
+                payData.body = '啾哩Brunch'
+                payData.total_fee = param['money'] * 100
+
+                var payfee = require("./../utils/wxpay");
+                async function Call() {
+                    var e = await payfee(payData)
+                    data = e
+                }
+                await Call()
+
+            } catch (err) {
+                if (err.code) {
+                    response = tool.error.ErrorSQL;
+                    log.warn(name, "code:", err.code, ", sql:", err.sql);
+                } else {
+                    log.warn(name, JSON.stringify(response));
+                    response = tool.error.ErrorCatch;
+                }
+            }
+        }
+
+        if (response.code != tool.error.OKCode) {
+            log.warn(name, JSON.stringify(response));
+        }
+
+        tool.MakeResponse(200,
+            {
+                res: response,
+                data: data,
+                action: "add_order",
+            }, res);
+        tool.log.debug("RestaurantPayfee::Run.out");
+    }
+}
+
+module.exports = RestaurantPayfee;
--- shop/server/utils/wxpay.js
+++ shop/server/utils/wxpay.js
@@ -35,14 +35,14 @@ async function payfee(data = {}) {
         // 商品简单描述 如：腾讯充值中心-QQ会员充值
         const productIntro = data.body
         //通知地址  确保外网能正常访问
-        const notifyUrl = 'https://www.minidope.com/index.php/home/PubWXPay/notify'
+        const notifyUrl = 'https://www.youyueworld.com/index.php/home/PubWXPay/notify'
         // 生成商家内部自定义的订单号, 商家内部的系统用的, 理论上只要不和其他订单重复, 使用任意的字符串都是可以的
         const tradeId = getTradeId(attach)
         // 生成签名
         // 这里是在 express 获取用户的 ip, 因为使用了 nginx 的反向代理, 所以这样获取
         // let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
         // ip = ip.match(/\d+\.\d+\.\d+\.\d+/)[0]
-        let ip = '47.105.87.66' //终端IP
+        let ip = '120.79.94.138' //终端IP
         // let ip = '127.0.0.1:9001'
         const sign = getPrePaySign(appId, attach, productIntro, mchId, nonceStr, notifyUrl, openId, tradeId, ip, price)
         // 将微信需要的数据拼成 xml 发送出去
