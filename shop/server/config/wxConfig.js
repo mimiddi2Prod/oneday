1bd6c1083e4e7765e3e6a19c55bbfd14cd16155e
mimiddi <1490533119@qq.com>
2019-09-17 15:23:55 +0800

    1

--- shop/server/config/wxConfig.js
+++ shop/server/config/wxConfig.js
@@ -2,7 +2,7 @@ const appid = 'wxa0869cd59ebaaa06'  // 啾哩好物
 const secret = '3a5fa1f990812a7cc9aa1a832fbc1f86'
 
 const mch_id = "1545776151" // 商户号
-const PAY_API_KEY = 'kz5fECdUC51XgocMYKY06Ktg5hxXz0P6' // 商户密钥
+const PAY_API_KEY = '8955558ee139ab858ec66cae41e36828' // 商户密钥
 
 module.exports = {
     appid: appid,
