ebb4ce1d55e8672c0aca057914dc08f220ddc1bf
jianping <1490533119@qq.com>
2019-09-20 18:46:39 +0800

    1

--- restaurant/server/apis/yinbao_add_onLineOrder.js
+++ restaurant/server/apis/yinbao_add_onLineOrder.js
@@ -76,7 +76,7 @@ async function yinbaoAddOnLineOrder(data = {}) {
                 "appId": appId,
                 "payMethod": "Wxpay",
                 "payOnLine": 1,
-                "orderRemark": (data.style == 0 ? "堂食" : "外带"),
+                "orderRemark": (data.style == 0 ? "堂食 " : "外带 ") + '桌号:' + data.restaurantTableName.toString(),
                 "dinnersNumber": data.dinnersNumber,
                 "orderDateTime": current_time,
                 "deliveryType": deliveryType,
@@ -92,7 +92,7 @@ async function yinbaoAddOnLineOrder(data = {}) {
                 "customerNumber": data.customerNumber, // 会员号
                 "payMethod": "CustomerBalance",
                 //"payOnLine": 1,
-                "orderRemark": (data.style == 0 ? "堂食" : "外带"),
+                "orderRemark": (data.style == 0 ? "堂食 " : "外带 ") + '桌号:' + data.restaurantTableName.toString(),
                 "dinnersNumber": data.dinnersNumber,
                 "orderDateTime": current_time,
                 "deliveryType": deliveryType,
