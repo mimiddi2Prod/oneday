f3df4772554bc4116965757172839b3d5e0b0ea8
jianping <1490533119@qq.com>
2019-10-18 18:33:51 +0800

    1

--- restaurant/server/apis/restaurant_wxPay_notify.js
+++ restaurant/server/apis/restaurant_wxPay_notify.js
@@ -20,57 +20,62 @@ function RestaurantWxPayNotify() {
             let openid = e.xml.openid[0]
             let totalPrice = Number(e.xml.total_fee[0]) * 0.01 // 获取的值 1 = 0.01
 
-            // 更改支付状态
-            sql = 'update restaurant_goods_order set pay_status = ? where trade_id = ? and open_id = ?'
-            row = await query(sql, [0, tradeId, openid])
-
             // 查询对应订单用于给银豹推送订单
             sql = 'select * from restaurant_goods_order where trade_id = ? and open_id = ?'
             row = await query(sql, [tradeId, openid])
             console.info(row)
+
             if (row.length > 0) {
-                // customerUid 更新积分时使用
-                let customerUid = row[0].customer_uid
+                let rowData = row
+                if(rowData[0].pay_status == 1){
+                    // 更改支付状态
+                    sql = 'update restaurant_goods_order set pay_status = ? where trade_id = ? and open_id = ?'
+                    row = await query(sql, [0, tradeId, openid])
 
-                let param = {}
-                param.openid = openid
-                param.style = row[0].style
-                param.dinnersNumber = row[0].dinners_number
-                param.restaurantTableName = row[0].table_number
-                param.payMethod = 'Wxpay'
-                param.cart = row.map(function (eData) {
-                    eData.goodsId = eData.goods_id
-                    eData.goodsParam = eData.param
-                    return eData
-                })
+                    // customerUid 更新积分时使用
+                    let customerUid = rowData[0].customer_uid
 
-                var yinbaoAddOrder = require("./yinbao_add_onLineOrder")
-                let AddOrderCall = await yinbaoAddOrder(param)
+                    let param = {}
+                    param.openid = openid
+                    param.style = rowData[0].style
+                    param.dinnersNumber = rowData[0].dinners_number
+                    param.restaurantTableName = rowData[0].table_number
+                    param.payMethod = 'Wxpay'
+                    param.cart = rowData.map(function (eData) {
+                        eData.goodsId = eData.goods_id
+                        eData.goodsParam = JSON.parse(eData.param)
+                        return eData
+                    })
 
-                // todo 写本地日志保存 用于保存银豹订单推送的情况  code:0 成功，1 失败
-                if (AddOrderCall.code == 0) {
-                    let yinbao_orderNo = AddOrderCall.orderNo
-                    sql = 'update restaurant_goods_order set yinbao_order_no = ? where trade_id = ? and open_id = ?'
-                    row = await query(sql, [yinbao_orderNo, tradeId, openid])
-                } else if (AddOrderCall.code == 1) {
+                    var yinbaoAddOrder = require("./yinbao_add_onLineOrder")
+                    let AddOrderCall = await yinbaoAddOrder(param)
 
-                }
-
-                // todo 推送银豹订单 微信支付时银豹不计算积分 需自行更新
-                if (customerUid) {
-                    let updateCustomerData = {}
-                    updateCustomerData.customerUid = customerUid
-                    updateCustomerData.balanceIncrement = 0
-                    updateCustomerData.pointIncrement = totalPrice
-                    let updateCustomer = require('./yinbao_update_customer')
-                    let updateCustomerCall = await updateCustomer(param)
-                    // todo 微信支付更新积分成功和失败的提醒 code:0 成功，1 失败
-                    if (updateCustomerCall.code == 0) {
-
-                    } else {
+                    // todo 写本地日志保存 用于保存银豹订单推送的情况  code:0 成功，1 失败
+                    if (AddOrderCall.code == 0) {
+                        let yinbao_orderNo = AddOrderCall.orderNo
+                        sql = 'update restaurant_goods_order set yinbao_order_no = ? where trade_id = ? and open_id = ?'
+                        row = await query(sql, [yinbao_orderNo, tradeId, openid])
+                    } else if (AddOrderCall.code == 1) {
 
                     }
+
+                    // todo 推送银豹订单 微信支付时银豹不计算积分 需自行更新
+                    if (customerUid) {
+                        let updateCustomerData = {}
+                        updateCustomerData.customerUid = customerUid
+                        updateCustomerData.balanceIncrement = 0
+                        updateCustomerData.pointIncrement = totalPrice
+                        let updateCustomer = require('./yinbao_update_customer')
+                        let updateCustomerCall = await updateCustomer(param)
+                        // todo 微信支付更新积分成功和失败的提醒 code:0 成功，1 失败
+                        if (updateCustomerCall.code == 0) {
+
+                        } else {
+
+                        }
+                    }
                 }
+
             }
         }
     }
