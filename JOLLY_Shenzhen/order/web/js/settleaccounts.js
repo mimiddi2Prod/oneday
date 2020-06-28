b884254a30dcaa38c7cde41d8dfd7e9ab4e4ad5e
mimiddi <1490533119@qq.com>
2020-06-28 17:33:31 +0800

    1

--- JOLLY_Shenzhen/order/web/js/settleaccounts.js
+++ JOLLY_Shenzhen/order/web/js/settleaccounts.js
@@ -1,10 +1,22 @@
 var settleaccountsvm = new Vue({
     el: "#settleaccounts",
     data: {
-
+        keyboard: [["1", "4", "7", "0"], ["2", "5", "8", "00"], ["3", "6", "3", "."], ["50", "100", "200", "300"], ["X", "确认"]],
+        total_price: ""
     },
     methods: {
+        hitKeyboard(keyboard, m) {
+            if (m <= 2) {
+                this.total_price = this.total_price.toString() + keyboard
+            } else if (m == 3) {
+                this.total_price = Number(this.total_price) + Number(keyboard)
+            } else if (keyboard == "X") {
+                let len = this.total_price.toString().length
+                this.total_price = len > 0 ? this.total_price.toString().substring(0, len - 1) : ""
+            }
 
+            console.info(this.total_price)
+        }
     },
     mounted: function () {
 
