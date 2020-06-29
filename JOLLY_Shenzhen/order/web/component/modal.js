c59457753d0d61cc16dd7adbbea96388897b7224
mimiddi <1490533119@qq.com>
2020-06-29 15:48:00 +0800

    1

--- JOLLY_Shenzhen/order/web/component/modal.js
+++ JOLLY_Shenzhen/order/web/component/modal.js
@@ -44,45 +44,50 @@ Vue.component('my-modal-1', {
 })
 
 // 折扣
-Vue.component('my-modal-discount', {
-    data: function () {
-        return {
-            list: ['95 折', '9 折', '85 折', '8 折', '75 折', '7 折', '6 折', '5 折', '免单', '抹零']
-        }
-    },
-    template: '<div class="modal fade" id="modal_2" tabindex="-1" role="dialog" aria-labelledby="myModal2" data-backdrop="static">\n' +
-        '    <div class="modal-dialog" role="document">\n' +
-        '        <div class="modal-content">\n' +
-        '            <div class="modal-header">\n' +
-        '                <h5 class="modal-title"></h5>\n' +
-        '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
-        '                    <span aria-hidden="true">&times;</span>\n' +
-        '                </button>\n' +
-        '            </div>\n' +
-        '            <div class="modal-body row">\n' +
-        '                <div class="input-group mb-3 w-100 align-items-center mx-5">\n' +
-        '                    <input type="text" class="form-control text-right" placeholder=""\n' +
-        '                           aria-label="Example text with button addon"\n' +
-        '                           aria-describedby="button-addon1"\n' +
-        '                           oninput="value=value.replace(/[^\\d.]/g,\'\')">\n' +
-        '                    <div class="input-group-prepend text-center ml-2">\n' +
-        '                        <span>%</span>\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '                <div class="nav mx-5 flex-row">\n' +
-        '                    <div class="text-center col-6 mb-2" v-for="item in list">\n' +
-        '                        <button class="btn btn-info py-2 px-4 w-100"><h5>{{item}}</h5></button>\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '            </div>\n' +
-        '            <div class="modal-footer">\n' +
-        '                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>\n' +
-        '                <button type="button" class="btn btn-primary" id="modal_2_submit">确定</button>\n' +
-        '            </div>\n' +
-        '        </div>\n' +
-        '    </div>\n' +
-        '</div>'
-})
+// Vue.component('my-modal-discount', {
+//     data: function () {
+//         return {
+//             list: ['95 折', '9 折', '85 折', '8 折', '75 折', '7 折', '6 折', '5 折', '免单', '抹零'],
+//             total_price: settleaccountsvm.total_price
+//         }
+//     },
+//     template: '<div class="modal fade" id="modal_2" tabindex="-1" role="dialog" aria-labelledby="myModal2" data-backdrop="static">\n' +
+//         '    <div class="modal-dialog" role="document">\n' +
+//         '        <div class="modal-content">\n' +
+//         '            <div class="modal-header">\n' +
+//         '                <h5 class="modal-title"></h5>\n' +
+//         '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
+//         '                    <span aria-hidden="true">&times;</span>\n' +
+//         '                </button>\n' +
+//         '            </div>\n' +
+//         '            <div class="modal-body row">\n' +
+//         '                <div class="mx-5 flex-row w-100 text-right">' +
+//         '                   <h5>当前￥{{total_price}}</h5>\n' +
+//         '                   <h5>折扣￥{{total_price}}</h5>\n' +
+//         '                </div>\n' +
+//         '                <div class="input-group mb-3 w-100 align-items-center mx-5">\n' +
+//         '                    <input type="text" class="form-control text-right" placeholder=""\n' +
+//         '                           aria-label="Example text with button addon"\n' +
+//         '                           aria-describedby="button-addon1"\n' +
+//         '                           oninput="value=value.replace(/[^\\d.]/g,\'\')">\n' +
+//         '                    <div class="input-group-prepend text-center ml-2">\n' +
+//         '                        <span>%</span>\n' +
+//         '                    </div>\n' +
+//         '                </div>\n' +
+//         '                <div class="nav mx-5 flex-row">\n' +
+//         '                    <div class="text-center col-6 mb-2" v-for="item in list">\n' +
+//         '                        <button class="btn btn-info py-2 px-4 w-100" onclick="getDiscountToCalculation({{item}})"><h5>{{item}}</h5></button>\n' +
+//         '                    </div>\n' +
+//         '                </div>\n' +
+//         '            </div>\n' +
+//         // '            <div class="modal-footer">\n' +
+//         // '                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>\n' +
+//         // '                <button type="button" class="btn btn-primary" id="modal_2_submit">确定</button>\n' +
+//         // '            </div>\n' +
+//         '        </div>\n' +
+//         '    </div>\n' +
+//         '</div>'
+// })
 Vue.component('get-discount', {
     data: function () {
         return {}
@@ -99,17 +104,17 @@ function showModalDiscount() {
         modal.find('.modal-title').text('折扣')
         // modal.find('.modal-body').text('是否退出登录？')
     })
-    $('#modal_2').on('hidden.bs.modal', function (e) {
-        $('#modal_2_submit')[0].removeEventListener("click", submit1);
-    })
+    // $('#modal_2').on('hidden.bs.modal', function (e) {
+    //     $('#modal_2_submit')[0].removeEventListener("click", submit1);
+    // })
     $('#modal_2').modal('show');
-    $('#modal_2_submit')[0].addEventListener("click", submit1)
+    // $('#modal_2_submit')[0].addEventListener("click", submit1)
 }
 
-function submit1() {
-    $('#modal_2').modal('hide');
-    console.info(333)
-    settleaccountsvm.getDiscountToCalculation(333444)
-}
+// function submit1() {
+//     $('#modal_2').modal('hide');
+//     console.info(333)
+//     settleaccountsvm.getDiscountToCalculation(333444)
+// }
 
 new Vue({el: '#MyModal'})
--- JOLLY_Shenzhen/order/web/js/settleaccounts.js
+++ JOLLY_Shenzhen/order/web/js/settleaccounts.js
@@ -2,30 +2,65 @@ var settleaccountsvm = new Vue({
     el: "#settleaccounts",
     data: {
         keyboard: [["1", "4", "7", "0"], ["2", "5", "8", "00"], ["3", "6", "9", "."], ["50", "100", "200", "300"], ["<-", "确认"]],
-        total_price: "894"
+        discount_list: [95, 9, 85, 8, 75, 7, 6, 5],
+        type: 0,
+        total_price: 894.32,
+        discount_price: "",
+        pay_price: "",
+        change_price: "",
     },
     methods: {
         hitKeyboard(keyboard, m) {
-            if (m <= 2) {
-                this.total_price = this.total_price.toString() + keyboard
-                if (!Number(this.total_price)) {
-                    this.total_price = 0
+            let price;
+            switch (this.type) {
+                case 0: {
+                    price = this.total_price
+                    calculationPrice()
+                    this.total_price = price
+                    break;
                 }
-            } else if (m == 3) {
-                this.total_price = Number(this.total_price) + Number(keyboard)
-            } else if (keyboard == "<-") {
-                let len = this.total_price.toString().length
-                this.total_price = len > 0 ? this.total_price.toString().substring(0, len - 1) : ""
+                case 1: {
+                    price = this.pay_price
+                    calculationPrice()
+                    this.pay_price = price
+                    break;
+                }
+                default:
+                    break;
             }
 
-            console.info(this.total_price)
+            function calculationPrice() {
+                if (m <= 2) {
+                    price = price.toString() + keyboard
+                    if (!Number(price)) {
+                        price = 0
+                    }
+                } else if (m == 3) {
+                    price = Number(price) + Number(keyboard)
+                } else if (keyboard == "<-") {
+                    let len = price.toString().length
+                    price = len > 0 ? price.toString().substring(0, len - 1) : ""
+                }
+            }
+
+            // 找零计算
+            this.change_price = 0 - (Math.ceil((this.total_price - this.pay_price) * 100) / 100)
         },
-        getDiscountToCalculation(e){
-            console.info(e)
+        getDiscountToCalculation(e) {
+            if (e == "抹零") {
+                this.discount_price = this.total_price - (this.total_price % 1)
+            } else {
+                this.discount_price = (this.total_price * (e.toString().length == 1 ? e * 10 : e)) / 100
+            }
+            // 超出小数2位数，向上取整
+            this.discount_price = Math.ceil(this.discount_price * 100) / 100
         },
+        getCurrentInput(index) {
+            this.type = index
+        }
     },
     mounted: function () {
-
+        this.pay_price = this.total_price
     },
     created: function () {
 
