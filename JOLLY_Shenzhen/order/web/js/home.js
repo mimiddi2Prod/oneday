d04f02d387cd69cd3a44372928fe677d949524c6
mimiddi <1490533119@qq.com>
2020-06-30 15:30:31 +0800

    添加商品

--- JOLLY_Shenzhen/order/web/js/home.js
+++ JOLLY_Shenzhen/order/web/js/home.js
@@ -20,12 +20,12 @@ var homevm = new Vue({
             price: 12,
             img_url: '../images/logo.png'
         }, {
-            id: 1,
+            id: 2,
             name: "白斩鸡白斩鸡白斩鸡白斩鸡",
             price: 12,
             img_url: '../images/logo.png'
         }, {
-            id: 1,
+            id: 3,
             name: "白斩鸡白斩鸡白斩鸡白斩鸡",
             price: 12,
             img_url: '../images/logo.png'
@@ -36,22 +36,33 @@ var homevm = new Vue({
             name: "白斩鸡白斩鸡白斩鸡白斩鸡",
             price: 12,
             discount: "",
-            discount_price: "",
+            discount_price: 12,
             num: 1,
             remark: '多放亿点辣椒',
             subtotal: 12
         }, {
-            id: 1,
+            id: 2,
             name: "白斩鸡白斩鸡白斩鸡白斩鸡",
             price: 14,
             discount: 90,
-            discount_price: "",
+            discount_price: 12.6,
             num: 2,
             remark: '大分的，同样多放亿点辣椒',
             subtotal: 28
         }],
         // 用于临时存放更改的商品
-        tempOrderDetail: {}
+        tempDiscount: "",
+        tempNum: 1,
+        tempOrderDetail: {
+            id: null,
+            name: "",
+            price: null,
+            discount: "",
+            discount_price: null,
+            num: null,
+            remark: "",
+            subtotal: null
+        }
     },
     methods: {
         toSettleAccounts() {
@@ -61,25 +72,67 @@ var homevm = new Vue({
         toOrderForm() {
             window.location.href = "orderform"
         },
-        showModalProduct(item) {
-            this.tempOrderDetail = item
-            if (!this.tempOrderDetail.discount) {
-                this.tempOrderDetail.discount_price = this.tempOrderDetail.price
-                this.tempOrderDetail.num = 1
+        showModalProduct(item, orderIndex) {
+            let temp = item
+            temp.orderIndex = orderIndex || null
+            if (!temp.discount) {
+                temp.discount = ""
+                temp.remark = ""
+                temp.discount_price = temp.price
+                temp.num = temp.num || 1
             }
-            this.tempOrderDetail.subtotal = this.tempOrderDetail.discount_price * this.tempOrderDetail.num
+            temp.subtotal = temp.discount_price * temp.num
+            // 初始化赋值
+            this.tempDiscount = ""
+            this.tempNum = 1
+            this.tempOrderDetail = Object.assign({}, temp) // 新建对象，防止vue引用赋值
             $('#modal_3').on('show.bs.modal', function (e) {
                 let modal = $(this)
                 modal.find('.modal-title').text('点单详细-' + item.name)
             })
             $('#modal_3').modal('show');
         },
+        calculationDiscount() {
+            console.info()
+        },
         changeOrder() {
-            $('#loading').modal('show');
+            // $('#loading').modal('show');
             $('#modal_3').modal('hide');
-            setTimeout(() => {
-                $('#loading').modal('hide');
-            }, 2000)
+            let self = this, temp = self.tempOrderDetail
+            if (temp.orderIndex) {
+                // 编辑
+            } else {
+                // 添加
+                delete temp.orderIndex
+                let haveSame = self.order.some(value => {
+                    return (value.id == temp.id && value.discount == temp.discount && value.remark == temp.remark)
+                })
+                temp.discount_price = temp.discount ? (Number(temp.discount) / 100) * temp.price : temp.price
+                if (haveSame) {
+                    self.order = self.order.map(value => {
+                        if (value.id == temp.id && value.discount == temp.discount && value.remark == temp.remark) {
+                            value.num = Number(value.num) + Number(temp.num)
+                            value.subtotal = Number(value.num) * value.discount_price
+                        }
+                        return value
+                    })
+                } else {
+                    temp.subtotal = temp.discount_price * temp.num
+                    self.order.push(temp)
+                }
+            }
+            // $('#loading').modal('hide');
+        }
+    },
+    watch: {
+        tempDiscount(val, oldVal) {
+            this.tempOrderDetail.discount = val
+            this.tempOrderDetail.discount_price = val ? this.tempOrderDetail.price * (Number(val) / 100) : this.tempOrderDetail.price
+            this.tempOrderDetail.subtotal = this.tempOrderDetail.discount_price * Number(this.tempOrderDetail.num)
+        },
+        tempNum(val) {
+            this.tempOrderDetail.num = val
+            this.tempOrderDetail.subtotal = this.tempOrderDetail.discount_price * Number(this.tempOrderDetail.num)
         }
     },
     mounted: function () {
