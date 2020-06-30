8cf4f9d900eda5a1c2885777d5736e326f3b9211
mimiddi <1490533119@qq.com>
2020-06-30 13:12:08 +0800

    1

--- JOLLY_Shenzhen/order/web/js/home.js
+++ JOLLY_Shenzhen/order/web/js/home.js
@@ -30,19 +30,28 @@ var homevm = new Vue({
             price: 12,
             img_url: '../images/logo.png'
         }],
+        // 预下单，discount（0-100，100为原价）有值时计算折扣价，
         order: [{
             id: 1,
             name: "白斩鸡白斩鸡白斩鸡白斩鸡",
             price: 12,
+            discount: "",
+            discount_price: "",
             num: 1,
-            remark: '多放亿点辣椒'
+            remark: '多放亿点辣椒',
+            subtotal: 12
         }, {
             id: 1,
             name: "白斩鸡白斩鸡白斩鸡白斩鸡",
             price: 14,
+            discount: 90,
+            discount_price: "",
             num: 2,
-            remark: '大分的，同样多放亿点辣椒'
-        }]
+            remark: '大分的，同样多放亿点辣椒',
+            subtotal: 28
+        }],
+        // 用于临时存放更改的商品
+        tempOrderDetail: {}
     },
     methods: {
         toSettleAccounts() {
@@ -51,10 +60,39 @@ var homevm = new Vue({
         },
         toOrderForm() {
             window.location.href = "orderform"
+        },
+        showModalProduct(item) {
+            this.tempOrderDetail = item
+            if (!this.tempOrderDetail.discount) {
+                this.tempOrderDetail.discount_price = this.tempOrderDetail.price
+                this.tempOrderDetail.num = 1
+            }
+            this.tempOrderDetail.subtotal = this.tempOrderDetail.discount_price * this.tempOrderDetail.num
+            $('#modal_3').on('show.bs.modal', function (e) {
+                let modal = $(this)
+                modal.find('.modal-title').text('点单详细-' + item.name)
+            })
+            $('#modal_3').modal('show');
+        },
+        changeOrder() {
+            $('#loading').modal('show');
+            $('#modal_3').modal('hide');
+            setTimeout(() => {
+                $('#loading').modal('hide');
+            }, 2000)
         }
     },
     mounted: function () {
-
+        // this.showModalProduct({
+        //     id: 1,
+        //     name: "白斩鸡白斩鸡白斩鸡白斩鸡",
+        //     price: 14,
+        //     discount: "",
+        //     discount_price: 14,
+        //     num: 2,
+        //     remark: '大分的，同样多放亿点辣椒',
+        //     subtotal: 28
+        // })
     },
     created: function () {
 
